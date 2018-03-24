fdfef4a6d7a494a9981f12f2fcc495ce44adf2f4
Emerson-Wenzel <emerson.wenzel@tufts.edu>
2018-03-24 09:04:37 -0400

    added __init__.py

--- server.py
+++ server.py
@@ -3,17 +3,13 @@ import requests
 import json
 
 import base64
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
-
-@app.route('/index')
-def index():
-    return "Hello, World!"
-
 
 @app.route('/postImage', methods=['POST'])
 def postImage():
@@ -54,4 +50,5 @@ def test():
     return jsonify({'key': 'Value'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
