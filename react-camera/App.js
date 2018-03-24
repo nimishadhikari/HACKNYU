fdc695281f12976de1576a40dcd884db173a5dee
Nimish Adhikari <nimishadhikari@gmail.com>
2018-03-24 00:40:34 -0400

    adding react camera stuff

---
+++ react-camera/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Slider, Vibration } from 'react-native';
+import {Constants, Camera, FileSystem, Permissions } from 'expo';
+
+export default class CameraExample extends React.Component {
+        state = {
+                        hasCameraPermission: null,
+                        type: Camera.Constants.Type.back,
+                };
+
+        async componentWillMount() {
+                const { status } = await Permissions.askAsync(Permissions.CAMERA);
+                this.setState({ hasCameraPermission: status === 'granted' });
+        }
+
+        render() {
+                const { hasCameraPermission } = this.state;
+                if (hasCameraPermission === null) {
+                        return <View />;
+                } else if (hasCameraPermission === false) {
+                        return <Text>No access to camera</Text>;
+                } else {
+                        return (
+                                <View style={{ flex: 1 }}>
+                                <Camera
+                                 style={{ flex: 1 }}
+                                 type={this.state.type}
+                                 ratio={'16:9'}>
+                                 <View
+                                 style={{
+                                         flex: 1,
+                                         backgroundColor: 'transparent',
+                                         flexDirection: 'row',
+                                 }}>
+                                 <TouchableOpacity
+                                 style= {[styles.flipButton,
+                                         styles.picButton,
+                                         { flex: 0.3, alignSelf: 'flex-end' }]}
+                                           /*onPress={this.takePicture.bind(this)}*/>
+                                           <Text style={styles.flipText}> SNAP </Text>
+                                </TouchableOpacity>
+                                </View>
+                                </Camera>
+                                </View>
+                        );
+                }
+        }
+}
+
+const styles = StyleSheet.create({
+        flipButton: {
+                flex: 0.3,
+                height: 40,
+                marginHorizontal: 2,
+                marginBottom: 10,
+                marginTop: 20,
+                borderRadius: 8,
+                borderColor: 'white',
+                borderWidth: 1,
+                padding: 5,
+                alignItems: 'center',
+                justifyContent: 'center',
+        },
+        flipText: {
+                color: 'white',
+                fontSize: 15,
+        },
+        picButton: {
+                backgroundColor: 'darkseagreen',
+        }
+});
